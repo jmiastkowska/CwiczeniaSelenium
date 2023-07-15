9e1f210adfe4f0fe20ec211d5ea5f98d3832a116
aska <j_miastkowska@o2.pl>
2023-07-15 15:23:07 +0200

    updateControl

--- src/main/java/p02/ControlFlowDemo.java
+++ src/main/java/p02/ControlFlowDemo.java
@@ -2,7 +2,9 @@ package p02;
 
 public class ControlFlowDemo {
     public static void main(String[] args) {
-       System.out.println(sum(3,5)) ;
+       System.out.println(sum(3,5));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(9));
     }
     /*
     sum(0,3)->6
@@ -16,8 +18,20 @@ public class ControlFlowDemo {
         return sum;
     }
 
+    public static boolean isPrime(int n){
+        for(int i=2;i<n;i++){   //2,3,4,5,6
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
     public static int fibo(int n){
-        return 0;
+        return 0; //todo
     }
 
 
