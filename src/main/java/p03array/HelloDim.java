e7da6cd9cd5591b273094e215c664d59e6653890
jmiastko <j_miastkowska@o2.pl>
2023-07-23 10:51:17 +0200

    array

---
+++ src/main/java/p03array/HelloDim.java
@@ -0,0 +1,26 @@
+package p03array;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class HelloDim {
+    public static void main(String[] args) {
+        int[] intArray1 = new int[6];
+        //{0,0,0,0,0,0}
+        intArray1[0]=6; //przypisanie idneksu do 1 elementu
+        intArray1[1]=5;
+        //...
+        intArray1[5]=1;
+
+        int[] intArray2 = {6,5,4,3,2,1}; // przypisanie wartosci
+
+        //printing
+        System.out.println(intArray2);  //hash
+
+        //accessing value
+        System.out.println(intArray2[2]);
+        //printing manualy
+        for(int index= 0; index < intArray2.length; index++){
+            System.out.print(intArray2[index]);
+        }
+    }
+}
