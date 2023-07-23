4545a714f2af2ec3e65b28f50ab216c435e12952
jmiastko <j_miastkowska@o2.pl>
2023-07-23 18:30:30 +0200

    gameCd

--- src/main/java/p03array/Game.java
+++ src/main/java/p03array/Game.java
@@ -1,6 +1,7 @@
 package p03array;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
     static char[][] board = {
@@ -9,8 +10,7 @@ public class Game {
             {' ', ' ', ' '}
     };
 
-
-
+static char currentSymbol = 'X';
 
     public static void printBoard(){
         for (int row=0; row<board.length; row++){
@@ -18,9 +18,32 @@ public class Game {
         }
         System.out.println("   1  2  3");
     }
+
+    public static void move(){
+        System.out.println("Runda "+ currentSymbol);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wpisz numer wiersza");
+        int rowNo = sc.nextInt();
+        System.out.println("Wpisz numer kolumny");
+        int colNo = sc.nextInt();
+        board[rowNo-1][colNo-1]=currentSymbol;
+        //2currentSymbol = przeciwnySymbol;
+        if(currentSymbol=='X'){
+            currentSymbol='O';
+        }else{
+            currentSymbol='X';
+        }
+    }
+
+
+
     public static void main(String[] args) {
       //  int wiek = readInt( "Podaj wiek");
 
-printBoard();
+        printBoard();
+        move();
+        printBoard();
+        move();
+        printBoard();
     }
 }
