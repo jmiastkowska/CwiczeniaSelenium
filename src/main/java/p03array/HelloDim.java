453ce7e3477f3f0ccdf05a81392e3202c7a31753
jmiastko <j_miastkowska@o2.pl>
2023-07-23 11:11:59 +0200

    arrayPrint

--- src/main/java/p03array/HelloDim.java
+++ src/main/java/p03array/HelloDim.java
@@ -5,6 +5,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class HelloDim {
     public static void main(String[] args) {
         int[] intArray1 = new int[6];
+        int[] array = {6,5,4,3,2,1};
         //{0,0,0,0,0,0}
         intArray1[0]=6; //przypisanie idneksu do 1 elementu
         intArray1[1]=5;
@@ -20,7 +21,13 @@ public class HelloDim {
         System.out.println(intArray2[2]);
         //printing manualy
         for(int index= 0; index < intArray2.length; index++){
-            System.out.print(intArray2[index]);
+            System.out.print(intArray2[index] + ",");
+        }
+        System.out.println();
+
+        //for(zmienna do przechowywania obecnego eleementu : struktura danych po ktorej mozna iterowac)
+        for( int element: array) {
+            System.out.print(element+ ",");
         }
     }
 }
