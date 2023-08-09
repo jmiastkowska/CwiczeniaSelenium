dff6a2492d2aa9a87b86007d6f2f23f3fdce4ad8
aska <j_miastkowska@o2.pl>
2023-08-09 21:11:25 +0200

    Loan

--- src/main/java/p04_oop/LoanCalculator.java
+++ src/main/java/p04_oop/LoanCalculator.java
@@ -6,10 +6,29 @@ public class LoanCalculator {
     double interestRate;
     String type;
 
+    static String[] AVAILABLE_TYPES= {"EQUAL","DESCENDING"};
     public LoanCalculator(double amount, int noOfMonth, double interestRate, String type) {
         this.amount = amount;
         this.noOfMonth = noOfMonth;
         this.interestRate = interestRate;
         this.type = type;
     }
+
+    public double totalCost(){
+        if(type == LoanTypes.EQUALS){
+            return -1;
+        } else if(type == LoanTypes.EQUALS){
+            return -2;
+        }
+        return -3;
+    }
+
+    public Instalment[][]generateSchedule(){
+        return null;
+    }
+
+    public static double getInstalmentPart(double[] instalmentData, String part){
+
+    }
+
 }
