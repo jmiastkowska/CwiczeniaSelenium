59d21e6f83b113b0ad1500ff5780a85100fab4cb
aska <j_miastkowska@o2.pl>
2023-08-11 20:24:21 +0200

    loanCD

--- src/main/java/p04_oop/LoanCalculator.java
+++ src/main/java/p04_oop/LoanCalculator.java
@@ -5,6 +5,8 @@ public class LoanCalculator {
     int noOfMonth;
     double interestRate;
     String type;
+    static double capitalPartAggregator = 0;
+    static double currentInterestPart = 0;
 
     static String[] AVAILABLE_TYPES= {"EQUAL","DESCENDING"};
     public LoanCalculator(double amount, int noOfMonth, double interestRate, String type) {
@@ -14,17 +16,54 @@ public class LoanCalculator {
         this.type = type;
     }
 
-    public double totalCost(){
-        if(type == LoanTypes.EQUALS){
-            return -1;
-        } else if(type == LoanTypes.EQUALS){
+    public static double totalCost(LoanCalculator loanCalculator){
+        if(loanCalculator.type == LoanTypes.EQUALS){
+
+            return loanInstalment(loanCalculator) * loanCalculator.noOfMonth;
+        } else if(loanCalculator.type == LoanTypes.DESCENDING){
             return -2;
         }
         return -3;
     }
 
-    public Instalment[][]generateSchedule(){
-        return null;
+    public static double moneyRemainToPay(LoanCalculator loanCalculator, int monthNum){
+        return totalCost(loanCalculator) - (monthNum) * loanInstalment(loanCalculator);
+    }
+
+
+
+    public Instalment[]generateSchedule(){
+        Instalment[] outcome =new Instalment[noOfMonth];
+        int instalmentNo;
+        double remainToPay, interestPart, capitalPart;
+        for(int i=0; i<noOfMonth; i++) {
+            instalmentNo = i + 1;
+            remainToPay = moneyRemainToPay(this, i);
+            interestPart = loanInstalmentInterestPart();
+            capitalPart = loanInstalmentCapitalPart();
+            outcome[i]= new Instalment(instalmentNo, remainToPay, interestPart, capitalPart);
+        }
+        return outcome;
+    }
+
+    public static double loanInstalment(){
+        double a = this.amount;
+        double b = this.interestRate;
+        double n = this.noOfMonth;
+        return (a*(b/12)*Math.pow(1+(b/12),n))/(Math.pow(1+(b/12),n)-1);
+    }
+
+    private double loanInstalmentCapitalPart(LoanCalculator loanCalculator) {
+        double result = loanInstalment(this) - currentInterestPart;
+        capitalPartAggregator += result;
+        return result;
+
+    }
+
+    private double loanInstalmentInterestPart(LoanCalculator loanCalculator) {
+        double result = (amount - capitalPartAggregator) * interestRate/12;
+        currentInterestPart = result;
+        return result;
     }
 
     public static double getInstalmentPart(double[] instalmentData, String part){
