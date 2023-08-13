06591874785c5b9d4c223f9c399cb685a2d832c2
aska <j_miastkowska@o2.pl>
2023-08-13 16:31:56 +0200

    enumCd

--- src/main/java/p04_oop/HelloEnum/Month.java
+++ src/main/java/p04_oop/HelloEnum/Month.java
@@ -1,5 +1,20 @@
 package p04_oop.HelloEnum;
 
 public enum Month {
-    JAN, FEB, MAR, APR, NOV, DEC
+    JAN(31,1),
+    FEB(28,2),
+    MAR(31,3),
+    APR(30,4),
+    NOV(30,11),
+    DEC(31,12);
+
+    private int days,no;
+    Month(int days, int no){
+        this.days = days;
+        this.no = no;
+    }
+
+    public int daysCount(){
+        return days;
+    }
 }
