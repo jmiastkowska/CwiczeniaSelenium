3c3f31b624b9401b5887354a3bebb570e692f9d3
aska <j_miastkowska@o2.pl>
2023-08-15 12:09:23 +0200

    cd list

---
+++ src/main/java/p04_oop/interfaces/stringExample/Main.java
@@ -0,0 +1,41 @@
+package p04_oop.interfaces.stringExample;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] names = {"Adam","Ewa","Michal","Adi","eWa"};
+        printWomenOnly(names);
+        System.out.println("----------");
+        printMenOnly(names);
+        System.out.println("----------");
+        printNamesOnly(names);
+    }
+
+    public static void printWomenOnly(String[]names){
+        for (String name:names){
+            if(name.endsWith("a")){
+                System.out.println(name);
+            }
+        }
+    }
+    public static void printMenOnly(String[]names){
+        for (String name:names){
+            if(!name.endsWith("a")){
+                System.out.println(name);
+            }
+        }
+    }
+
+    public static void printNamesOnly(String[]names){
+        for (String name:names){
+            //EwA
+            // e,E ->e==E
+            //wA,wa-> wA ==wa
+            if(name.substring(0,1).equals(name.substring(0,1).toUpperCase()) &&
+                    name.substring(1).equals(name.substring(1).toLowerCase())
+        ){
+                System.out.println(name);
+            }
+        }
+    }
+
+}
