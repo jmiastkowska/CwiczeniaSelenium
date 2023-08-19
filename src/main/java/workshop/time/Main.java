3b8d2aef564bfdd5341f852526028e897bc430b8
aska <j_miastkowska@o2.pl>
2023-08-19 15:34:24 +0200

    newBranch

---
+++ src/main/java/workshop/time/Main.java
@@ -0,0 +1,13 @@
+package workshop.time;
+
+public class Main {
+    public static void main(String[] args) {
+        Time time1 =new Time("13:25:59");
+        System.out.println(time1);  //13:25:59
+        time1.addMinutes(3);
+        System.out.println(time1); //to nie pomylka -inutable
+        Time time2 = time1.addMinutes(3);
+        System.out.println(time2);  //13:28:59
+        System.out.println(time2.addHours(12)); //01:28:59
+    }
+}
---
+++ src/main/java/workshop/time/Time.java
@@ -0,0 +1,25 @@
+package workshop.time;
+
+public class Time {
+
+    private int hours, minutes, seconds;
+
+    public void addSeconds(int seconds) {
+
+    }
+
+    public void addMinutes(int minutes) {
+
+    }
+
+    public void addHours(int hours) {
+
+    }
+
+    
+
+
+
+
+
+}
