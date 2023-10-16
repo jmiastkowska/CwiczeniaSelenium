ef1ea4cb59be990679d8f801f77ee177a3b9bf32
aska <j_miastkowska@o2.pl>
2023-10-16 21:27:15 +0200

    zadDomowe

--- src/main/java/workshop/time/zadanieDomowe/Cart.java
+++ src/main/java/workshop/time/zadanieDomowe/Cart.java
@@ -13,16 +13,35 @@ public class Cart implements Iterable<Product> {
        addProduct(productToAdd);
     }
 
-    public void getTotalSum() {
+    public void addProduct(Product productToAdd, int amount) {
+        for (int i = 0; i < amount; i++) {
+            products.add(productToAdd);
+        }
+    }
+    public double getTotalSum() {
+        double totalSum = 0;
+        for (Product product : products) {
+            totalSum += product.getPrice();
+        }
+        return totalSum;
     }
 
     public void getProductsList() {
     }
 
-    public void deleteProduct(String cheese) {
+    public void deleteProduct(String productName) {
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getName().equals(productName)) {
+                iterator.remove();
+                break;
+            }
+        }
     }
 
     public void deleteAllProducts() {
+        products.clear();
     }
 
     @Override
--- src/main/java/workshop/time/zadanieDomowe/Main.java
+++ src/main/java/workshop/time/zadanieDomowe/Main.java
@@ -11,28 +11,30 @@ public class Main {
 
         Cart cart = new Cart();
 
-        cart.addProduct(cheese); // By default adds 1 product
-/*
+        cart.addProduct(cheese, 1); // By default adds 1 product
+
         cart.addProduct(shoe, 3); // You can add a product many times to the basket
 
 
 
-        cart.getTotalSum() // 1x10 + 3x15 = 10 + 45 = 55
-
+        cart.getTotalSum(); // 1x10 + 3x15 = 10 + 45 = 55
+/*
 
 
         cart.getProductsList() // zwraca string "cheese ::: 1 unit ::: 10; shoe ::: 3 units ::: 45"
 
 
 
-        cart.deleteProduct("cheese")
+
 
         cart.getTotalSum() // 45
 
 
+*/
+        cart.deleteProduct("cheese");
+        cart.deleteAllProducts();
 
-        cart.deleteAllProducts()
-
+/*
         cart.getTotalSum() // 0
 
         cart.getProductsList() // zwraca string "There are no items in your cart"
