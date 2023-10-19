371d4a7a9dc8c84edfb69bdb65bd87d89cf555d8
aska <j_miastkowska@o2.pl>
2023-10-19 20:56:29 +0200

    upTime

--- src/test/java/workshop/time/TimeTest.java
+++ src/test/java/workshop/time/TimeTest.java
@@ -1,10 +1,10 @@
 package workshop.time;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import workshop.time.TimeCwicz.Time;
 
 import java.util.stream.Stream;
 
